9d43db27931aa1265ff1a2ffb9afc66aca8791d7
Cross <miami.hype1@gmail.com>
2026-07-02 16:59:52 -0400

    chore: set base path for GitHub Pages hosting

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  // Served from https://crossfiber.github.io/plant-locker/
+  base: '/plant-locker/',
   plugins: [react()],
 })
